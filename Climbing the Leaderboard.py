0a2e6f285b29dd9b9860d1df908aca4ca82c65fd
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-04-13 14:56:14 -0700

    Add files via upload

---
+++ Climbing the Leaderboard.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+def climbingLeaderboard(ranked, player):
+    # Write your code here
+    ranked = list(set(ranked))
+    ranked.sort()
+    i = 0
+    n = len(ranked)
+    res = []
+    
+    for score in player:
+        while i < n and ranked[i] <= score:
+            i += 1
+        res.append(n - i + 1)
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ranked_count = int(input().strip())
+
+    ranked = list(map(int, input().rstrip().split()))
+
+    player_count = int(input().strip())
+
+    player = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(ranked, player)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
