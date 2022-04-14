bee5faf2619ee8d8f0cd2cb09cac37e7392c8096
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-04-14 10:54:10 -0700

    Add files via upload

---
+++ Ice Cream Parlor.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    # Write your code here
+    n = len(arr)
+    result = []
+
+    
+    for i in range(n - 1, 0, -1):
+        if arr[i] < m:
+            for j in range(i):
+                temp = arr[i] + arr[j]
+                if temp == m:
+                    result = [j + 1, i + 1]
+                    break
+    return result
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
